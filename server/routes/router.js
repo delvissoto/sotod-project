29b7f2e24f664e1bec70c0668d170f57e4b3728d
delvissoto <delvissoto99@icloud.com>
2024-01-30 06:15:03 +0000

    slice and form added

--- server/routes/router.js
+++ server/routes/router.js
@@ -2,16 +2,10 @@ const express = require('express')
 
 
 const router = express.Router()
-router.get('/something', (req, res) =>{
-    const somethingData = [
-        {
-            "id":"1",
-            "myName":"Delvis", 
-            "lastName":"Soto"
-        }
-    ]
-    res.send(somethingData)
+// router.get('/something', (req, res) =>{
+    
+   
 
-})
+// })
 module.exports = router
 
--- src/App.js
+++ src/App.js
@@ -1,10 +1,42 @@
 
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [user, setUser] = useState('')
+  const [password, setPassword] = useState('')
+  const [email, setEmail] = useState('')
+  const [address, setAddress]= useState('')
+
+  const handleSubmit = (e) =>{
+    e.preventDefaul()
+    if(user && password && email && address){
+
+    }
+
+  }
+
+
   return (
     <div className="App">
-  <p>hi</p>
+      <section>
+        <form>
+          <label htmlFor='user'>User: </label>
+          <input type='text' name='user' value={user} onChange={(e) => setUser(e.target.value)}required/><br/>
+
+          <label htmlFor='password'>Password: </label>
+          <input type='password' name='password' value={password} onChange={(e) => setPassword(e.target.value)}required/><br/>
+
+          <label htmlFor='email'>Email: </label>
+          <input type='email' name='email' value={email} onChange={(e) => setEmail(e.target.value)}required/><br/>
+
+          <label htmlFor='address'>Address: </label>
+          <input type='text' name='address' value={address} onChange={(e) => setAddress(e.target.value)}required/><br/>
+
+          <input type='submit' value="create account"/>
+
+        </form>
+      </section>
     </div>
   );
 }
